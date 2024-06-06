a2ff64549d0f75170682a3ea26ca9fc90bc37c87
Johaner <providencesatterfield@gmail.com>
2024-06-06 12:54:22 +0900

    changed user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,44 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Helpers\Utils;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['username', 'password', 'role'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public static function getAvailableFields($client, $role, $op)
+    {
+        $mapRolesToFields = [
+            'web' => [
+                'id' => [
+                    'r' => ['admin'],
+                ],
+                'username' => [
+                    'r' => ['admin'],
+                ],
+                'role' => [
+                    'r' => ['admin'],
+                ],
+                'created_at' => [
+                    'r' => ['admin'],
+                ],
+            ]
+        ];
+        return Utils::getAvailableFields($mapRolesToFields, $client, $role, $op);
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public static function getConditionsForAvailableRows($client, $user): array
+    {
+        if ($client === 'web') {
+            if ($user->role === 'admin') {
+                return [['role', '!=', 'admin']];
+            }
+        }
+        return [false];
+    }
 }
